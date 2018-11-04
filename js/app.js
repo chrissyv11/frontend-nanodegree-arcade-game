02ba5edbdecc3463bae63ca593c106cd1febe42d
Christine <cvendikos@gmail.com>
2018-11-04 14:46:36 -0600

    Create Player Class and add methods. Set initial location.

--- js/app.js
+++ js/app.js
@@ -2,18 +2,23 @@
 var Enemy = function() {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // CV Set initial location
+    this.x = 10;
+    this.y = 10;
+    // CV Set speed
+    this.speed = 5;
 };
 
-// Update the enemy's position, required method for game
+// CV Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    // CV Handle collision with the Player
 };
 
 // Draw the enemy on the screen, required method for game
@@ -25,11 +30,47 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
+var Player = function() {
+    this.sprite = 'images/char-cat-girl.png';
+    // CV Set initial location
+    this.x = 10;
+    this.y = 10;
+};
+
+// CV Update the player's position, required method for game
+// Parameter: dt, a time delta between ticks
+Player.prototype.update = function(dt) {
+    // You should multiply any movement by the dt parameter
+    // which will ensure the game runs at the same speed for
+    // all computers.
+    // CV Handle collision with the Player
+};
+
+// Draw the player on the screen
+Player.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+// CV Handle user input on player.  Receive user input, allowedKeys
+//(the key which was pressed) and move the player accordingly (left, right)
+// Player cannot move off screen (check for that and handle)
+// When player reaches water, game is reset by moving player to initial location
+// Write a separate reset Player method
+Player.prototype.handleInput = function
 
 // Now instantiate your objects.
+// Create several new Enemies and place them in an array called allEnemies
 // Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
+// CV Place the player object in a variable called player
+const player = new Player ();
+const enemy1 = new Enemy ();
+const enemy2 = new Enemy ();
+const enemy3 = new Enemy ();
+player.render();
+enemy1.render();
+enemy2.render();
+enemy3.render();
+const allEnemies = [enemy1, enemy2, enemy3];
 
 
 // This listens for key presses and sends the keys to your
