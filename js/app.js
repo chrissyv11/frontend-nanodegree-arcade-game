ebef9a2d41e441b4335de0265eebc6239b6d3560
Christine <cvendikos@gmail.com>
2018-11-11 14:26:32 -0600

    Added cat-girl image to engine file. Added variable speed and position parameters. Added function to handle keyboard input.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,28 @@
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
     // CV Set initial location
-    this.x = 10;
-    this.y = 10;
+    this.x = x;
+    this.y = y;
     // CV Set speed
-    this.speed = 5;
+    this.speed = speed;
 };
 
 // CV Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
+    // Multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += this.speed * dt;
+    this.speed = Math.floor(Math.random() * 3 + 1);
+    if(this.x > 505){
+      this.x = 0;
+    }
     // CV Handle collision with the Player
 };
 
@@ -33,8 +38,11 @@ Enemy.prototype.render = function() {
 var Player = function() {
     this.sprite = 'images/char-cat-girl.png';
     // CV Set initial location
-    this.x = 10;
-    this.y = 10;
+    this.x = 252;
+    this.y = 596;
+    // Set length of step
+    this.horizStep = 101;
+    this.vertStep = 83;
 };
 
 // CV Update the player's position, required method for game
@@ -46,6 +54,10 @@ Player.prototype.update = function(dt) {
     // CV Handle collision with the Player
 };
 
+Player.prototype.resetPlayer = function(){
+  this.x = 252;
+  this.y = 596;
+}
 // Draw the player on the screen
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
@@ -56,9 +68,30 @@ Player.prototype.render = function() {
 // CV Player cannot move off screen (check for that and handle)
 // CV When player reaches water, game is reset by moving player to initial location
 // CV Write a separate reset Player method
-Player.prototype.handleInput = function{
-  
-}
+Player.prototype.handleInput = function(input) {
+  switch(input){
+    case 'left':
+      if (this.x > 0){
+          this.x -= this.horizStep;
+      }
+      break;
+      case 'right':
+      if (this.x < 404) {
+        this.x += this.horizStep;
+      }
+      break;
+      case 'up':
+      if (this.y > 0) {
+          this.y -= this.vertStep;
+      }
+      break;
+      case 'down':
+      if (this.y < 332) {
+          this.y += this.vertStep;
+      }
+      break;
+    }
+};
 
 // Now instantiate your objects.
 // Create several new Enemies and place them in an array called allEnemies
@@ -68,10 +101,10 @@ const player = new Player ();
 const enemy1 = new Enemy ();
 const enemy2 = new Enemy ();
 const enemy3 = new Enemy ();
-player.render();
-enemy1.render();
-enemy2.render();
-enemy3.render();
+//player.render();
+//enemy1.render();
+//enemy2.render();
+//enemy3.render();
 const allEnemies = [enemy1, enemy2, enemy3];
 
 
